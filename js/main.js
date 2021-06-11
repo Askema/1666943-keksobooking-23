ffaa631bffc8ea0bbab46c0e4c05c430f4ad96aa
Dmitry <readmanga8@gmail.com>
2021-06-11 11:17:41 +0300

    Правки

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 // функцию взял из https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 // Дополнил методом toFixed
 
-function getRandomIntFromRange(min, max, roundPlus) {
+function getRandomIntFromRange(value1, value2, roundPlus) {
+  let min = Math.min(value1, value2);
+  let max = Math.max(value1, value2);
   if (arguments.length === 3) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return (Math.random() * (max - min + 1)  + min).toFixed(roundPlus);
+    return Number((Math.random() * (max - min) + min).toFixed(roundPlus));
   } else {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -14,4 +14,3 @@ function getRandomIntFromRange(min, max, roundPlus) {
 }
 
 getRandomIntFromRange();
-
