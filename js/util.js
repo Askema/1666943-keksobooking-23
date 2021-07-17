fdb53452bbc0650bb23b1b0177a6ddaef5a4cd08
Dmitry <readmanga8@gmail.com>
2021-07-17 18:20:03 +0300

    Добавляет функции фильтрации и debounce

--- js/util.js
+++ js/util.js
@@ -1,5 +1,17 @@
 const ESC = 'Esc';
 const ESCAPE = 'Escape';
+const DEFAULT_FILTER = 'any';
+
+const priceCategories = {
+  middle: 'middle',
+  low: 'low',
+  high: 'high',
+};
+
+const pricesRange = {
+  low: 10000,
+  high: 50000,
+};
 
 function getRandomIntFromRange(value1, value2, roundPlus) {
   let min = Math.min(value1, value2);
@@ -17,5 +29,39 @@ const roundOff = (number) => Number(number.toFixed(5));
 
 const esc = (evt) => evt.key === ESC || evt.key === ESCAPE;
 
-export { getRandomIntFromRange, roundOff, esc};
+function debounce (callback, timeoutDelay = 500) {
+
+  let timeoutId;
+
+  return (...rest) => {
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+}
+
+const isMatchedFilter = (findings, filterValue) => String(findings) === String(filterValue) || filterValue === DEFAULT_FILTER;
+
+const isMatchedPrice = (findings, filterValue) => {
+  if (filterValue === priceCategories.low) {
+    return findings < pricesRange.low;
+  }
+  if (filterValue === priceCategories.middle) {
+    return findings >= pricesRange.low && findings < pricesRange.high;
+  }
+  if (filterValue === priceCategories.high) {
+    return findings >= pricesRange.high;
+  }
+  return true;
+};
+
+const isMatchedFeatures = (findings) => {
+  const checkedFeatures = document.querySelectorAll('input:checked');
+  return Array.from(checkedFeatures).every((feature) => {
+    if (findings) {
+      return findings.includes(feature.value);
+    }
+  });
+};
+export { getRandomIntFromRange, roundOff, esc, debounce, isMatchedFilter, isMatchedPrice, isMatchedFeatures};
 
